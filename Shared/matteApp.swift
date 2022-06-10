784663275cb8e217eb517d33224bb69718ced11c
Jeff Reiner <theundeadghost@gmail.com>
2022-06-10 19:07:13 +0200

    Initial Commit

---
+++ Shared/matteApp.swift
@@ -0,0 +1,17 @@
+//
+//  matteApp.swift
+//  Shared
+//
+//  Created by Jeff Reiner on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct matteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
