35fc7501582f0aaa4faa2c6203f5316580abeb0f
Jeff Reiner <theundeadghost@gmail.com>
2022-06-10 19:30:19 +0200

    Add PillButton

---
+++ Shared/PillButton.swift
@@ -0,0 +1,40 @@
+//
+//  Button.swift
+//  matte
+//
+//  Created by Jeff Reiner on 6/10/22.
+//
+
+import SwiftUI
+
+struct PillButton: View {
+    var text: String
+    var onClick: () -> Void
+    
+    var body: some View {
+        Button(action: onClick) {
+            Text(text)
+                .foregroundColor(.black)
+                .padding(.horizontal, 22)
+                .padding(.vertical, 10)
+                .background(.white)
+                .cornerRadius(.greatestFiniteMagnitude)
+        }
+    }
+}
+
+struct PillButton_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PillButton(text: "Print Hello", onClick: { print("Hello") })
+                .previewLayout(PreviewLayout.sizeThatFits)
+                .padding()
+                .background(.black)
+            
+            PillButton(text: "Print Hello", onClick: { print("Hello") })
+                .previewLayout(PreviewLayout.sizeThatFits)
+                .padding()
+                .background(.purple)
+        }
+    }
+}
