a3e78142edbb6ee80c82652a77d56a95fe38ca04
Jeff Reiner <theundeadghost@gmail.com>
2022-06-10 22:31:30 +0200

    Implement initial matting feature with white matt, added in maxSize to 1000 which is upscaled to @3x for Retina. Might need to find a better solution here.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,99 @@
 //
 
 import SwiftUI
+import UIKit
+import func AVFoundation.AVMakeRect
 
 struct ContentView: View {
+    @State private var image: Image?
+
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Color.pink
+            .ignoresSafeArea()
+            .overlay(
+                VStack {
+                    Spacer()
+                    
+                    image?
+                       .resizable()
+                       .scaledToFit()
+                    
+                    Spacer()
+
+                    HStack {
+                        PillButton(text: "Browse", onClick: { showingImagePicker = true })
+                        
+                        Spacer()
+                        
+                        if image != nil {
+                            PillButton(text: "Save", onClick: save)
+                            
+                            PillButton(text: "New", onClick: { image = nil })
+                        }
+                    }
+                }
+                    .padding([.horizontal, .bottom])
+                .onChange(of: inputImage) { _ in loadImage() }
+                .sheet(isPresented: $showingImagePicker) {
+                    ImagePicker(image: $inputImage)
+                }
+            )
+            .accentColor(Color.black)
+    }
+    
+    func matteImage(image: UIImage) -> UIImage {
+        var maxSize = max(image.size.height, image.size.width)
+        
+        if (maxSize >= 1000) {
+            maxSize = 1000
+        }
+        
+        let size = CGSize(width: maxSize, height: maxSize)
+        
+        let renderer = UIGraphicsImageRenderer(size: size)
+
+        return renderer.image { ctx in
+            // @todo Make this dynamic
+            UIColor.white.setFill()
+            
+            ctx.fill(CGRect(origin: .zero, size: size))
+            
+            let rect = AVMakeRect(aspectRatio: image.size, insideRect: CGRect(origin: .zero, size: size))
+
+            ctx.cgContext.setFillColor(UIColor.white.cgColor)
+
+            image.draw(in: rect)
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        
+        processedImage = matteImage(image: inputImage)
+        
+        guard let processedImage = processedImage else { return }
+                
+        image = Image(uiImage: processedImage)
+    }
+
+    func save() {
+        guard let processedImage = processedImage else { return }
+
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+
+        imageSaver.errorHandler = {
+            print("Oops! \($0.localizedDescription)")
+        }
+
+        imageSaver.writeToPhotoAlbum(image: processedImage)
     }
 }
 
--- Shared/PillButton.swift
+++ Shared/PillButton.swift
@@ -14,6 +14,7 @@ struct PillButton: View {
     var body: some View {
         Button(action: onClick) {
             Text(text)
+                .fontWeight(.semibold)
                 .foregroundColor(.black)
                 .padding(.horizontal, 22)
                 .padding(.vertical, 10)
